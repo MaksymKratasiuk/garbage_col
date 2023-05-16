ba144c510eab86b543d16e6006b3bb3c1b32ef50
MaksymKratasiuk <makshm1307@gmail.com>
2023-05-16 19:53:58 +0300

    Add project files.

---
+++ Task1.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+
+namespace garbage_col
+{
+    public class Film:IDisposable
+    {
+        public StringBuilder Film_Name { get; set; }
+        
+        public StringBuilder Studio_Name { get; set; }
+
+        public StringBuilder Genre { get; set; }
+
+        public StringBuilder Durability { get; set; }
+
+        public StringBuilder Year { get; set; }
+
+
+
+        public Film()
+        {
+            Film_Name = default;
+            Studio_Name = default;
+            Genre = default;
+            Durability = default;
+            Year = default;
+        }
+        public Film (string film_name, string studio_name, string genre, string durability,string year)
+        {
+            Film_Name = new StringBuilder(film_name);
+            Studio_Name = new StringBuilder(studio_name);
+            Genre = new StringBuilder(genre);
+            Durability = new StringBuilder(durability);
+            Year = new StringBuilder(year);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Film Name: " + Film_Name);
+            Console.WriteLine("Studio Name: " + Studio_Name);
+            Console.WriteLine("Genre: " + Genre);
+            Console.WriteLine("Durability: " + Durability);
+            Console.WriteLine("Year: " + Year);
+        }
+
+        public void Dispose ()
+        {
+            Film_Name = default;
+            Studio_Name = default;
+            Genre = default;
+            Durability = default;
+            Year = default;
+
+        }
+    }
+    internal class Task1
+    {
+        public static void task1()
+        {
+            using (Film film = new Film("The Shawshank Redemption", "Castle Rock Entertainment", "Drama", "2h 22min", "1994"))
+            {
+                film.Show();
+                // Інші операції з об'єктом Film
+                film.Dispose();
+                film.Show();
+            }
+            
+        }
+
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace garbage_col
+{
+    public class Play : IDisposable
+    {
+        public StringBuilder PlayName { get; set; }
+        public StringBuilder TheatreName{get;set;}
+        public StringBuilder Genre{get;set;}
+        public StringBuilder Duration{get;set;}
+        public List<string> Actors { get; set; }
+
+        public Play(string playName, string theatreName, string genre, string duration, List <string>actors)
+        {
+            PlayName = new StringBuilder(playName);
+            TheatreName = new StringBuilder(theatreName);
+            Genre = new StringBuilder(genre);
+            Duration = new StringBuilder(duration);
+            Actors = actors;
+        }
+
+       
+        public void ShowDetails()
+        {
+            Console.WriteLine("Play Name: " + PlayName);
+            Console.WriteLine("Theatre Name: " + TheatreName);
+            Console.WriteLine("Genre: " + Genre);
+            Console.WriteLine("Duration: " + Duration);
+            Console.WriteLine("Actors: " + string.Join(", ", Actors));
+        }
+
+        public void Dispose()
+        {
+            PlayName = default;
+            TheatreName = default;
+            Genre = default;
+            Actors = default;
+        }
+    }
+
+    internal class Task2
+    {
+        public static void task2()
+        {
+            List<string> actors = new List<string> { "Actor1", "Actor2", "Actor3" };
+
+            using (Play play = new Play("Hamlet", "Royal Theatre", "Tragedy", "3:10", actors))
+            {
+                play.ShowDetails();
+                // Інші операції з об'єктом Play
+            }
+        }
+    }
+}
